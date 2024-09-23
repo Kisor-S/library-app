aef2a3e70ed7d7d7fc162de64aa0d4678a024892
Kisor-S <kishorevk910@gmail.com>
2024-09-23 15:46:55 +0530

    Creat a function which pushes every book to the myLibrary array

--- script.js
+++ script.js
@@ -8,10 +8,19 @@ function book(name, author, pages, status) {
     this.info = function() {
         return this.name + " by " + this.author + ", "+ this.pages + "pages, " + this.status;
     }
+    addBookToLibrary(this);
 }
 
+function addBookToLibrary(book) {
+    myLibrary.push(`name: ${book.name}, author: ${book.author}, pages: ${book.pages}, status: ${book.status}`);
+}
+
+addBookToLibrary.prototype = book;
+
 const theBook = new book('theBook', 'theAuthor', '100', 'read');
+const aBook = new book('aBook', 'theAuthor', '100', 'read');
 
 console.log(theBook.info());
+console.log(myLibrary);
 
   
